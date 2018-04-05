1122982a480885ced31714a9c25710ec66ea62b0
Winton Welsh <mail@wintoni.us>
2018-04-05 17:06:52 -0400

    defaultTask and starterNode more fleshed out

--- packages/default-tasks/lib/index.js
+++ packages/default-tasks/lib/index.js
@@ -1,4 +1,28 @@
-export async function defaultTask({ tasks }) {
+import chalk from "chalk"
+
+const ignore = ["defaultTask", "setup", "teardown"]
+
+export async function defaultTask({ tasks }, { alias }) {
+  let output = "\n"
+
+  for (let task in tasks) {
+    if (ignore.includes(task)) {
+      continue
+    }
+
+    output += chalk.green(task)
+
+    if (alias[task]) {
+      for (let key in alias[task]) {
+        for (let option of alias[task][key]) {
+          output += ` --${option}`
+        }
+      }
+    }
+
+    output += "\n"
+  }
+
   // eslint-disable-next-line no-console
-  console.log(Object.keys(tasks).join("\n"))
+  console.log(output)
 }
--- packages/starter-tasks/lib/index.js
+++ packages/starter-tasks/lib/index.js
@@ -1,16 +1,26 @@
+export async function setup(config) {
+  config.alias.starterNode = {
+    b: ["branch"],
+    p: ["path"],
+  }
+
+  return config
+}
+
 export async function starterNode({ path, run }) {
-  await run("git", [
+  let clone = [
     "clone",
     "git@github.com:invrs/node-starter.git",
     path,
-  ])
+  ]
 
-  await run("sh", [
-    "-c",
-    `
+  await run("git", clone)
+
+  let rmGit = `
     cd ${path};
     rm -rf .git;
     git init .
-    `,
-  ])
+  `
+
+  await run("sh", ["-c", rmGit])
 }
